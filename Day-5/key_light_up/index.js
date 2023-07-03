4ff675ba59e4c3c835f2e2395efbbf4a76164431
santmailo <iamsunil006@gmail.com>
2023-07-03 22:46:55 +0530

    created a text color changer DOM

---
+++ Day-5/key_light_up/index.js
@@ -0,0 +1,15 @@
+
+const COLOR = 'rgb(128, 128, 255)'
+document.body.style.fontFamily = "sans-serif";  
+window.addEventListener("keydown",colorDevsnest);
+let listOfLetters = document.querySelectorAll(".letter");
+function colorDevsnest(event){
+  let letter = event.key.toUpperCase();
+  for(let item of listOfLetters){
+    let text = item.innerText.toUpperCase();
+      if(text==letter){
+        item.style.color = COLOR;
+      }
+  }
+
+}
